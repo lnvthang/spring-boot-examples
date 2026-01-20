f3e9a17da2d5d3589c02e8e53459fb47fe177d63
lnvthang <lnvthang.it@gmail.com>
2026-01-20 22:45:32 +0700

    initial project setup

---
+++ lab1/src/main/java/com/lab1/demo/base/HealthController.java
@@ -0,0 +1,15 @@
+package com.lab1.demo.base;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequiredArgsConstructor
+public class HealthController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Lab 1: " + LocalDateTime.now();
+    }
+}
